81df792db08a5efabfce5762d0e68afb417ea64d
Supawit Saeliew <63398464+JayJacka@users.noreply.github.com>
2024-09-02 00:02:01 +0700

    add text demo

---
+++ font_example_scene.py
@@ -0,0 +1,23 @@
+from manim import *
+
+class TextDemo(Scene):
+    def construct(self):
+        text = Text("Never gonna give you up")
+        text2 = Text("Never gonna let you down")
+
+        circle = Circle()
+        circle.set_fill(BLUE, opacity=0.5)
+        circle.set_stroke(BLUE, width=4)
+
+        square = Square()
+        square.set_fill(RED, opacity=0.5)
+        square.set_stroke(RED, width=4)
+        
+        self.play(Write(text))
+        self.play(Unwrite(text))
+        self.remove(text)
+        self.play(Write(text2))
+        self.play(Transform(text2, circle))
+        self.remove(text2)
+        self.play(Uncreate(circle))
+        self.play(Create(square)) 
