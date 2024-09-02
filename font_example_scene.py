d09707789f9825e821bfa0b60ee0d129aa46a10e
Supawit Saeliew <63398464+JayJacka@users.noreply.github.com>
2024-09-02 21:42:39 +0700

    add manim config file

--- font_example_scene.py
+++ font_example_scene.py
@@ -3,7 +3,9 @@ from manim import *
 class TextDemo(Scene):
     def construct(self):
         text = Text("Never gonna give you up")
+        text.set_color(RED)
         text2 = Text("Never gonna let you down")
+        text2.set_color(BLUE)
 
         circle = Circle()
         circle.set_fill(BLUE, opacity=0.5)
